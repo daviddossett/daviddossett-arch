22e8f8594222b4603f83dcf69baf8972a09f1356
mistople <ddossett@microsoft.com>
2021-01-20 22:27:13 -0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import headshot from '../src/images/headshot.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="content">
+        <section className="header">
+          <img src={headshot} className={'headshot'} alt='My headshot' />
+          <h1>David Dossett</h1>
+          <p className={'role'}>Designer & Developer</p>
+        </section>
+        <section>
+          <h2>Currently</h2>
+          <h3>Microsoft</h3>
+          <p>Senior Designer (2016 - Present)</p>
+        </section>
+        <section>
+          <h2>Projects</h2>
+          <h3>Coming soon</h3>
+        </section>
+        <section>
+          <h2>Figma plugins</h2>
+          <h3>Coming soon</h3>
+        </section>
+        <section className={'links'}>
+          <a href={'/'}>Twitter -&gt;</a>
+          <a href={'/'}>GitHub -&gt;</a>
+          <a href={'/'}>Instagram -&gt;</a>
+          <a href={'/'}>LinkedIn -&gt;</a>
+        </section>
+      </div>
     </div>
   );
 }
