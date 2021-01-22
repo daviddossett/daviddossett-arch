b8633918cb319dd35f6766691c5eb6a991b95b6f
mistople <ddossett@microsoft.com>
2021-01-21 20:49:36 -0800

    Removed duplicate text

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ function App() {
           <h2>Currently</h2>
           <h3>Microsoft</h3>
           <p>Senior Designer (2016 - Present)</p>
-          <p>Senior Designer (2016 - Present)</p>
         </section>
         <section>
           <h2>Projects</h2>
