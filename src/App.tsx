5d0e91908a09a0ef23cafe79acf6880387f6a5af
mistople <ddossett@microsoft.com>
2021-02-02 22:37:19 -0800

    Updated layout and added first project

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,22 @@ function App() {
       <div className="content">
         <section className="header">
           <img src={headshot} className={'headshot'} alt='My headshot' />
-          <h1>David Dossett</h1>
-          <p className={'role'}>Designer / developer</p>
+          <h1>&#128075;&nbsp;Hi, I'm David </h1>
+          <p className={'role'}>I'm a designer and developer at Microsoft</p>
         </section>
-        <section>
+        {/* <section>
           <h2>Currently</h2>
           <h3>Microsoft</h3>
           <p>Senior Designer (2016 - Present)</p>
+        </section> */}
+        <section>
+          <h2>Projects  &#128679;</h2>
+          <h3><a href={'https://filmandcolor.co'} target="blank">filmandcolor</a></h3>
+          <p>Color palettes generated from your favorite movie stills. Built with React, Clarifai, and more.</p>
+          <br/>
         </section>
         <section>
-          <h2>Projects</h2>
-          <h3>Coming soon</h3>
-        </section>
-        <section>
-          <h2>Figma plugins</h2>
-          <h3>Coming soon</h3>
+          <h2>Figma plugins &#128679;</h2>
         </section>
         <section className={'links'}>
           <a href={'https://twitter.com/david_dossett'} target="blank">Twitter -&gt;</a>
